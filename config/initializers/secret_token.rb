3ae62b04bb14f4b737be9e3a2989b18b075066fe
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-11-25 18:31:36 -0400

    first commit, everything running fine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmpcV2::Application.config.secret_token = '16ca47b90be2114fa407424095916e85d16234bf8dd22468a2f8b4a39bf19adf1d146d9a616801d4403a2d43af24fe8ebdf478adfb91957bef94e4fcd035c080'
