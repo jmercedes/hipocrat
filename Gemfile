9a4e502274700549a6ae2d6a4f1e0ccd79768029
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-12-07 08:45:01 -0400

    install and style inquiries gem

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ gem 'refinerycms', '~> 2.1.0'
 # Optionally, specify additional Refinery CMS Extensions here:
 gem 'refinerycms-acts-as-indexed', '~> 1.0.0'
 gem 'refinerycms-blog', '~> 2.1.0'
-#gem 'refinerycms-inquiries', '~> 2.1.0'
+gem 'refinerycms-inquiries', :git => 'git://github.com/refinery/refinerycms-inquiries.git'
 gem 'refinerycms-news', '~> 2.1.0'
 
 #  gem 'refinerycms-search', '~> 2.1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131207002156) do
+ActiveRecord::Schema.define(:version => 20131207120449) do
 
   create_table "refinery_images", :force => true do |t|
     t.string   "image_mime_type"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(:version => 20131207002156) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  create_table "refinery_inquiries_inquiries", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
+    t.boolean  "spam",       :default => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
+
+  add_index "refinery_inquiries_inquiries", ["id"], :name => "index_refinery_inquiries_inquiries_on_id"
 
   create_table "refinery_news_item_translations", :force => true do |t|
     t.integer  "refinery_news_item_id"
