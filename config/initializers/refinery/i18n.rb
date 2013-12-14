3c167804ee42e1295cee67df83599077c20bc1fb
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-12-13 21:09:00 -0400

    add locales

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -5,11 +5,11 @@ Refinery::I18n.configure do |config|
 
   # config.current_locale = :en
 
-  #config.default_frontend_locale = :es
+  config.default_frontend_locale = :es
 
   #config.frontend_locales = [:es]
   
-  #config.locales = {:es => "Español", :en => "English"}
+  config.locales = {:es => "Español", :en => "English"}
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
 
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
