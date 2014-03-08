24a7f4721b51ed58dd91c98a7f5dc06c26d487fc
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-03-08 15:16:26 -0400

    add image to job_posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def menu_header
-      menu_items = Refinery::Menu.new(Refinery::Page.menu_pages)
+      menu_items = Refinery::Menu.new(Refinery::Page.fast_menu)
+      #menu_items = Refinery::Menu.new(Refinery::Page.menu_pages)
 
       presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
       presenter.first_css = nil
