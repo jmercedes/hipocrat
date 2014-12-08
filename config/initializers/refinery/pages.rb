259779781cc2a3b3e1e5b3656d2f41773ec7f9cb
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-12-08 14:09:14 -0400

    add application layout to page array

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -45,7 +45,7 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  config.layout_template_whitelist = [:home, :show, :services, :board, :job, :bio_detail, :ecard_layout, :ecard]
+  config.layout_template_whitelist = [:application,:home, :show, :services, :board, :job, :bio_detail, :ecard_layout, :ecard]
 
   config.use_layout_templates = true
 
